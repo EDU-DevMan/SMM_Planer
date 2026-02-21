43ddbab71eda4fc9ee6513374b975055cc2ee1ce
Pavelwell7 <pavelbugaev303@gmail.com>
2026-02-21 20:03:29 +0300

    uptade SMM_planer_ok.py

--- SMM_planer_ok.py
+++ SMM_planer_ok.py
@@ -3,8 +3,10 @@ import json
 import time
 
 from ok_api import OkApiException
+from SMM_timers import get_publication_time_match, get_match_time_post_deleted
 from ok_config import ok_access, OK_GROUP_ID
 from SMM_google_parser import get_client_authorization, SPREADSHEET_NAME, WORKSHEET_NAME
+
 
 
 def upload_photo(image_url):
@@ -42,36 +44,75 @@ def post_to_ok(text, photo_token=None):
                 type="GROUP_THEME",
                 attachment=attachment
             )
-            return response
+            return response.json()
 
         except OkApiException as e:
-            print(f"   Ошибка API OK ({e.error_code}): {e.message}. Ждем 30 сек")
+            print(f"Ошибка API OK ({e.error_code}): {e.message}. Ждем 30 сек")
             time.sleep(30)
 
+
+def delete_from_ok(post_id):
+    try:
+        clean_id = str(post_id).replace('"', '').replace("'", "").strip()
+        response = ok_access.mediatopic.deleteTopic(
+            topic_id=clean_id
+        )
+        if hasattr(response, 'json'):
+            result = response.json()
+        else:
+            result = response
+        if isinstance(result, dict) and 'error_code' in result:
+            print(f"Ошибка ОК: {result.get('error_msg')}")
+            return False
+        return True
+
+    except OkApiException as e:
+        print(f"Ошибка API ({e.error_code}): {e.message}")
+        return False
 
 def main():
     client = get_client_authorization()
     sheet = client.open(SPREADSHEET_NAME).worksheet(WORKSHEET_NAME)
-    records = sheet.get_all_records()
     header = sheet.find("Статус OK")
     status_col_num = header.col
-    for i, row in enumerate(records, start=2):
-        ok_flag = row.get("OK")
-        status = row.get("Статус OK")
-        if str(ok_flag) == "1" and (status == "Запланировано" or status == ""):
-            text_post = row.get("Текст поста")
-            photo_url = row.get("Ссылка на фото")
-            photo_token = None
-            if photo_url:
-                print("Загружаю фото...")
-                photo_token = upload_photo(photo_url)
-                if not photo_token:
-                    print("Не удалось загрузить фото.")
-                    sheet.update_cell(i, status_col_num, "Ошибка")
-            post_to_ok(text_post, photo_token)
-            print("Успех")
-            sheet.update_cell(i, status_col_num, "Опубликовано")
-            break
+    while True:
+        records = sheet.get_all_records()
+        time_matches = get_publication_time_match()
+        publish_map = {item['Ids']: item['timing_post'] for item in time_matches}
+
+        delete_matches = get_match_time_post_deleted()
+        delete_map = {item['Ids']: item['timing_delete'] for item in delete_matches}
+
+        for i, row in enumerate(records, start=2):
+            ok_flag = str(row.get("OK"))
+            status = str(row.get("Статус OK"))
+            post_id = row.get("Ids")
+            if ok_flag == "1" and (status == "Запланировано" or status == ""):
+                text_post = row.get("Текст поста")
+                photo_url = row.get("Ссылка на фото")
+                if not publish_map.get(post_id):
+                    continue
+                photo_token = None
+                if photo_url:
+                    print("Загружаю фото...")
+                    photo_token = upload_photo(photo_url)
+                    if not photo_token:
+                        sheet.update_cell(i, status_col_num, "Ошибка")
+                        continue
+                res = post_to_ok(text_post, photo_token)
+                print("Успех")
+                sheet.update_cell(i, status_col_num, f"Опубликовано:{res}")
+                break
+
+            elif ok_flag == "1" and status.startswith("Опубликовано"):
+                if not delete_map.get(post_id):
+                    continue
+                raw_id = status.split(":")[1]
+                ok_post_id = raw_id.replace('"', '').strip()
+                delete_from_ok(ok_post_id)
+                sheet.update_cell(i, status_col_num, "Удалено")
+                break
+        time.sleep(60)
 
 if __name__ == "__main__":
     main()
