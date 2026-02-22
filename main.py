cb3141f30043f04d7965ed770e7302efb97f7e73
IPodstolnyy <ipodstolnyy@spb.beeline.ru>
2026-02-22 17:56:23 +0300

    text corrected

--- main.py
+++ main.py
@@ -7,11 +7,28 @@ from SMM_typography import main
 from SMM_google_parser import get_data_from_sheet
 
 
+"""
+    > Все комментарии и предложения из этого скрипта должны быть
+     перенесены в README|CONTRIBUTING при создании основного релиза
+"""
+
+"""
+    Описание модуля schedule > https://pypi.org/project/schedule/
+    Модуль позволяет запускать задания по расписанию.
+    Для тестирования запуска по расписанию выполняются два задания:
+    def run_smm_typography() и def run_get_data_from_sheet()
+    Реализована запись логов работы generates_log() 
+    в директорию logs - создается автоматически.
+    """
+
+# TODO: в основной реализации вместо тестовых заданий должны быть добавлены
+# модули SMM_planer_ok.py | SMM_planer_tg.py | SMM_planer_vk.py
+
 LOGS_PATH = "logs"
 
 
 def generates_log():
-    "Функция гененерирует логи работы schedule"
+    "Функция генерирует логи работы функция автозапуска"
 
     os.makedirs(LOGS_PATH, exist_ok=True)
 
@@ -27,23 +44,23 @@ def generates_log():
     return logging.getLogger(__name__)
 
 
+# TODO: Пример автозапуска SMM_typography.py модуля
 def run_smm_typography():
     """Запускает модуль typography.py"""
 
     generates_log().info("Корректируем текст")
     try:
-        # TODO: Взял для примера несколько модулей для запуска
         print(main())
         generates_log().info("Корректировка успешна")
     except Exception as e:
         generates_log().error(f"Ошибка - {e}")
 
 
+# TODO: Пример автозапуска SMM_google_parser.py модуля
 def run_get_data_from_sheet():
     """Запускает модуль SMM_google_parser.py"""
     generates_log().info("Читаем таблицу")
     try:
-        # TODO: Взял для примера несколько модулей для запуска
         print(get_data_from_sheet())
         generates_log().info("Таблица прочитана успешно")
     except Exception as e:
@@ -51,9 +68,8 @@ def run_get_data_from_sheet():
 
 
 def runs_tasks_schedule():
-    """Настройка расписания"""
+    """Запуск по расписанию"""
 
-    # TODO: Пример взял вот тут:  https://pypi.org/project/schedule/
     generates_log().info("SMM Planer запущен...")
 
     schedule.every(10).seconds.do(run_smm_typography)
@@ -69,6 +85,6 @@ if __name__ == "__main__":
     try:
         runs_tasks_schedule()
     except KeyboardInterrupt:
-        generates_log().info("Выполения остановлено")
+        generates_log().info("Выполнение остановлено")
     except Exception as e:
         generates_log().critical(f"Ошибка: {e}")
